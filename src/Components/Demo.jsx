b6ee706430b90dd8a819f633356835584a728ee6
SantiagoDoval <danieldovals@gmail.com>
2023-08-21 22:23:28 -0500

    First Version

--- src/Components/Demo.jsx
+++ src/Components/Demo.jsx
@@ -3,22 +3,47 @@ import { copy, linkIcon, loader, tick } from '../assets'
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-    
+
     const [article, setArticle] = useState({
-        url:'',
-        summary:'',
+        url: '',
+        summary: '',
     })
 
-    const [getSumary,{error,isFetching}]= useLazyGetSummaryQuery();
+    const [allArticles, setAllArticles] = useState([])
+    const [copied, setCopied] = useState("")
 
-    const handleSubmit = async (e) =>{
-        e.preventDefault();
-        const {data} = await getSumary({articleUrl:article.url})
+    const [getSumary, { error, isFetching }] = useLazyGetSummaryQuery();
 
-        if(data?.summary){
-            const newArticle ={...article,summary:data.summary}
-            setArticle(newArticle)            
+    useEffect(() => {
+        const articlesFromLocalStorage = JSON.parse(
+            localStorage.getItem('articlesAiSummarize')
+        )
+
+        if (articlesFromLocalStorage) {
+            setAllArticles(articlesFromLocalStorage)
         }
+    }, [])
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { data } = await getSumary({ articleUrl: article.url })
+        console.log(article)
+
+        if (data?.summary) {
+            const newArticle = { ...article, summary: data.summary }
+            const updatedAllArticles = [newArticle, ...allArticles]
+            setAllArticles(updatedAllArticles)
+            setArticle(newArticle)
+
+            localStorage.setItem('articlesAiSummarize', JSON.stringify(updatedAllArticles))
+        }
+    }
+
+    const handleCopy = (copyUrl) =>{
+        setCopied(copyUrl)
+        navigator.clipboard.writeText(copyUrl);
+        setTimeout(()=>setCopied(false),3000)
     }
 
     return (
@@ -34,7 +59,7 @@ const Demo = () => {
                         type="url"
                         placeholder="Enter a url"
                         value={article.url}
-                        onChange={(e) => {setArticle({...article,url:e.target.value}) }}
+                        onChange={(e) => { setArticle({ ...article, url: e.target.value }) }}
                         required
                         className="url_input peer"
                     />
@@ -46,8 +71,47 @@ const Demo = () => {
                     </button>
                 </form>
                 {/* Browser URL History */}
+                <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+                    {allArticles.map((item, index) => (
+                        <div
+                            key={`link-${index}`}
+                            onClick={() => setArticle(item)}
+                            className="link_card">
+                            <div className="copy_btn" onClick={()=>handleCopy(item.url)}>
+                                <img src={copied===item.url ? tick  : copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
+                            </div>
+                            <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                                {item.url}
+                            </p>
+                        </div>
+
+                    ))}
+                </div>
             </div>
             {/* Display Results  */}
+            <div className="m-y10 max-w-full flex justify-center items-center">
+                {isFetching ? (
+                    <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+                ): error ? (
+                    <p className="font-inter font-bold text-black text-center"> Well, that wasn't supossed to happen...
+                    <br />
+                    <span className="font-satoshi font-normal text-gray-700"> {error?.data?.error}</span>
+                    </p>
+                ):(
+                    article.summary && (
+                        <div className="flex flex-col gap-3">
+                            <h2 className="font-satoshi font-bold text-gray-600 text-xl mt-5 ">
+                                Article <span className="blue_gradient"> Summary</span>
+                            </h2>
+                            <div className="summary_box mb-5">
+                                <p className="font-inter font-medium text-sm text-gray-700">
+                                    {article.summary}
+                                </p>
+                            </div>
+                        </div>
+                    )
+                )}
+            </div>
         </section>
     )
 }
